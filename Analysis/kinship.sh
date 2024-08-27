18bdfe51f27ac5b37b911ceb57cb26a990d58133
nataliemallen <86844435+nataliemallen@users.noreply.github.com>
2024-08-27 12:18:08 -0400

    Update kinship.sh

--- Analysis/kinship.sh
+++ Analysis/kinship.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A fnrdewoody
+#SBATCH -A fnrpupfish
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -t 14-00:00:00
@@ -13,13 +13,13 @@ module load biocontainers
 module load angsd/0.935
 
 # Set your input BAM file list
-bam_list="/scratch/negishi/allen715/chicken_turtles/final_bams/bams_list.txt"
+#bam_list="/scratch/negishi/allen715/chicken_turtles/final_bams/bams_list.txt"
 
 # Set the reference genome FASTA file path
-ref_fasta="/scratch/negishi/allen715/chicken_turtles/reference/ref.fa"
+#ref_fasta="/scratch/negishi/allen715/chicken_turtles/reference/ref.fa"
 
 # Set output directory
-output_dir="/scratch/negishi/allen715/chicken_turtles/kinship/"
+#output_dir="/scratch/negishi/allen715/chicken_turtles/kinship/"
 
 # ### First generate a file with allele frequencies (angsdput.mafs.gz) and a file with genotype likelihoods (angsdput.glf.gz).
 # angsd -bam "$bam_list" -GL 2 -doMaf 1 -SNP_pval 1e-6 -doMajorMinor 1 -doGlf 3 -minmaf 0.05 -nThreads 64 \
@@ -29,4 +29,4 @@ output_dir="/scratch/negishi/allen715/chicken_turtles/kinship/"
 #zcat nobo.mafs.gz | cut -f5 |sed 1d >freq
 
 ### run NgsRelate -n is number of individuals
-/home/allen715/ngsrelate/ngsRelate/ngsRelate -g nobo.glf.gz -n 71 -f freq -O nobo_relate
+/home/allen715/ngsrelate/ngsRelate/ngsRelate -g nobo.glf.gz -n 89 -f freq -O nobo_relate
